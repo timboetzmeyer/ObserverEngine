413235b7c1f559ef1b21b3e256f830008983b81a
Tim Bötzmeyer <tim.boetzmeyer@gmx.de>
2017-03-10 19:39:57 +0100

    some javadoc comments

--- de.boetzmeyer.observerengine/src/de/boetzmeyer/observerengine/IObserverEngine.java
+++ de.boetzmeyer.observerengine/src/de/boetzmeyer/observerengine/IObserverEngine.java
@@ -1,13 +1,60 @@
 package de.boetzmeyer.observerengine;
 
+/**
+ * @author Tim
+ * 
+ *         This is interface for the interacting of a client application with
+ *         the observer engine.
+ *
+ */
 interface IObserverEngine {
 
+	/**
+	 * Method for adding an observer of a state that is known in the engine.
+	 * 
+	 * @param inState
+	 *            the state the observer is interested in
+	 * @param inStateObserver
+	 *            the implementation of the {@link IStateObserver} interface
+	 *            with the method stateChanged that is invoked on changes of the
+	 *            state
+	 * @return true, if the observer could be added to the engine
+	 */
 	boolean addStateChangeListener(final IState inState, final IStateObserver inStateObserver);
 
+	/**
+	 * Method for removing an observer of a state that is known in the engine.
+	 * 
+	 * @param inState
+	 *            the state the observer is interested in
+	 * @param inStateObserver
+	 *            the implementation of the {@link IStateObserver} interface
+	 *            with the method stateChanged that is invoked on changes of the
+	 *            state
+	 * @return true, if the observer could be removed from the engine
+	 */
 	boolean removeStateChangeListener(final IState inState, final IStateObserver inStateObserver);
 
+	/**
+	 * Method to update a state in the engine.
+	 * 
+	 * @param inState
+	 *            the state that has to be known in the engine
+	 * @param inValue
+	 *            the new value of the state that is going to be published by
+	 *            the engine
+	 * @return the number of direct observer calls for this state
+	 */
 	long update(final IState inState, final String inValue);
 
+	/**
+	 * Get the current value of the state.
+	 * 
+	 * @param inState
+	 *            the state the client application is interested in
+	 * @return the current value of the state or 'null' if the state does not
+	 *         exist in the engine
+	 */
 	String getValue(final IState inState);
 
 }
--- de.boetzmeyer.observerengine/src/de/boetzmeyer/observerengine/IObserverEngineAdmin.java
+++ de.boetzmeyer.observerengine/src/de/boetzmeyer/observerengine/IObserverEngineAdmin.java
@@ -3,22 +3,68 @@ package de.boetzmeyer.observerengine;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * @author Tim
+ * 
+ *         The administration interface allows to start and stop the engine. It
+ *         also allows the client to get statistical information about the model
+ *         of the engine.
+ *
+ */
 public interface IObserverEngineAdmin extends IObserverEngine {
 
+	/**
+	 * Starts the observer engine.
+	 */
 	void start();
 
+	/**
+	 * Stops the observer engine.
+	 */
 	void stop();
 
+	/**
+	 * Get a list of modified states since the given date.
+	 * 
+	 * @param inLastQuery
+	 *            the date to compare the changes
+	 * @return a list of modified dates since the given date
+	 */
 	List<IState> getModifiedStates(final Date inLastQuery);
-	
+
+	/**
+	 * Get all states that are known to the engine.
+	 * 
+	 * @return all states that are known to the engine
+	 */
 	List<IState> getStates();
-	
+
+	/**
+	 * Get all modules that are known to the engine.
+	 * 
+	 * @return all modules that are known to the engine
+	 */
 	List<IModule> getModules();
-	
+
+	/**
+	 * Get all observers of a state that are known to the engine.
+	 * 
+	 * @return all observers of a state that are known to the engine
+	 */
 	List<IObserver> getStateObservers(final IState inState);
-	
+
+	/**
+	 * Get all observers of a state in a module that are known to the engine.
+	 * 
+	 * @return all observers of a state in a module that are known to the engine
+	 */
 	List<IObserver> getStateObservers(final IState inState, final IModule inModule);
-	
+
+	/**
+	 * Get all observers of a module that are known to the engine.
+	 * 
+	 * @return all observers of a module that are known to the engine
+	 */
 	List<IObserver> getModuleObservers(final IModule inModule);
 
 }
